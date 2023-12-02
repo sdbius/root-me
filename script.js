babfd4794fbd3984f224be30bf88889956a0e2ce
tiagovst <tiago.vassan@gmail.com>
2023-12-01 23:21:44 -0300

    night mode

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const body = document.body;
+  const toggleModeButton = document.getElementById('toggleModeButton');
+
+  toggleModeButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    // Salve a preferência do usuário no modo local storage, se necessário
+    localStorage.setItem('darkModeEnabled', body.classList.contains('dark-mode'));
+  });
+
+  // Verifique se o usuário já tinha o modo escuro ativado
+  const darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+  if (darkModeEnabled) {
+    body.classList.add('dark-mode');
+  }
+});
